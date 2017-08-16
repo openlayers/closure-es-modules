f9539a94f2667e06ad8378f5d57ea22c2d9bab03
Tim Schaub <tim.schaub@gmail.com>
2017-08-16 10:49:09 -0400

    Updates

--- src/math/index.js
+++ src/math/index.js
@@ -2,6 +2,7 @@
  * Add two numbers together.
  * @param {number} a The first number.
  * @param {number} b The second number.
+ * @return {number} The sum.
  */
 export function add(a, b) {
   return a + b;
